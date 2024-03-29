65891b6be77a3fbb7ccb06bd8161f774a6da4374
VipWW <55458813+VipWW@users.noreply.github.com>
2024-03-29 10:21:26 +0100

    Playwright fix

--- playwright.config.ts
+++ playwright.config.ts
@@ -16,7 +16,7 @@ const config: PlaywrightTestConfig = {
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {
 		/* Base URL to use in actions like `await page.goto('/')`. */
-		baseURL: process.env.PUBLIC_ROOT_URL || 'http://localhost:5173',
+		baseURL: !process.env.CI ? 'http://localhost:5173' : 'http://localhost:3000',
 
 		launchOptions: {
 			slowMo: parseInt(process.env.SLOW_MO || '0')
