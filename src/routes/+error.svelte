2344ddbb0b8309815779a036855f43ee6f9b7171
VipWW <55458813+VipWW@users.noreply.github.com>
2024-03-30 22:03:19 +0100

    minor fix

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -8,7 +8,9 @@
 </svelte:head>
 
 <div class="flex flex-col items-center justify-center space-y-3">
-	<h1 class="text-4xl font-extrabold leading-none tracking-tight md:text-5xl lg:text-6xl">{get(page).status}</h1>
+	<h1 class="text-4xl font-extrabold leading-none tracking-tight md:text-5xl lg:text-6xl">
+		{get(page).status}
+	</h1>
 	<p class="text-lg">{get(page).error?.message}</p>
 	<a href="/" class="btn">Go back</a>
 </div>
