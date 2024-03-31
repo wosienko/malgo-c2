ab5ee4e0c6da29ace7c943ff90456bddebe63979
VipWW <55458813+VipWW@users.noreply.github.com>
2024-03-31 21:48:38 +0200

    minor fix

--- src/routes/login/+layout.server.ts
+++ src/routes/login/+layout.server.ts
@@ -2,5 +2,5 @@ import type { LayoutServerLoad } from './$types';
 import { redirect } from '@sveltejs/kit';
 
 export const load: LayoutServerLoad = ({ locals }) => {
-	if (locals.user) redirect(302, '/home');
+	if (locals.user && locals.session) redirect(302, '/home');
 };
