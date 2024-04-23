4491026f8e363caaa6c3e22f2008d150a40dcf91
VipWW <55458813+VipWW@users.noreply.github.com>
2024-04-23 10:47:17 +0200

    chore: moved session to seperate components

--- src/routes/(authorized)/projects/[id]/+layout.svelte
+++ src/routes/(authorized)/projects/[id]/+layout.svelte
@@ -6,8 +6,13 @@
 	import { get } from 'svelte/store';
 	import { onDestroy, onMount } from 'svelte';
 	import { afterNavigate } from '$app/navigation';
+	import Session from '$lib/components/custom/Session.svelte';
+	import SessionLoading from '$lib/components/custom/SessionLoading.svelte';
+	import SessionNotFound from '$lib/components/custom/SessionNotFound.svelte';
 
 	let { data } = $props();
+
+	let project = data.project!;
 
 	let currentUrl = $state(get(page).url.pathname);
 	afterNavigate(() => {
@@ -18,18 +23,6 @@
 		const d = new Date(date);
 		// format DD.MM.YYYY
 		return `${d.getDate().toString().padStart(2, '0')}.${(d.getMonth() + 1).toString().padStart(2, '0')}.${d.getFullYear()}`;
-	};
-
-	const olderThanInSeconds = (date: string, seconds: number): boolean => {
-		const d = new Date(date);
-		const now = new Date();
-		return (now.getTime() - d.getTime()) / 1000 > seconds;
-	};
-
-	const formatDateAndTime = (date: string): string => {
-		const d = new Date(date);
-		// format DD.MM.YYYY HH:MM:SS
-		return `${d.getDate().toString().padStart(2, '0')}.${(d.getMonth() + 1).toString().padStart(2, '0')}.${d.getFullYear()} ${d.getHours().toString().padStart(2, '0')}:${d.getMinutes().toString().padStart(2, '0')}:${d.getSeconds().toString().padStart(2, '0')}`;
 	};
 
 	const drawerResize = () => {
@@ -72,9 +65,9 @@
 		id="project-header"
 		class="flex w-full justify-around border-b-2 border-base-300 bg-base-200 py-2"
 	>
-		<span class="mt-1">{formatDate(data.project.startDate)}</span>
-		<a href={`/projects/${data.project.id}`} class="btn btn-ghost btn-sm">{data.project.name}</a>
-		<span class="mt-1">{formatDate(data.project.endDate)}</span>
+		<span class="mt-1">{formatDate(project.startDate)}</span>
+		<a href={`/projects/${project.id}`} class="btn btn-ghost btn-sm">{project.name}</a>
+		<span class="mt-1">{formatDate(project.endDate)}</span>
 	</div>
 	<div id="sidebar-wrap" class="max-w-full">
 		<Sidebar>
@@ -82,49 +75,29 @@
 			<svelte:fragment slot="menu">
 				{#await data.sessions}
 					<li>
-						<div class="flex w-full flex-col">
-							<div class="skeleton h-8 w-full"></div>
-							<div class="skeleton h-4 w-full"></div>
-							<div class="skeleton h-4 w-full"></div>
-						</div>
+						<SessionLoading />
 					</li>
-					{:then sessions}
+				{:then sessions}
 					{#if sessions.length === 0}
 						<li>
-							<div class="flex w-full flex-col">
-								<div class="text-center text-lg text-info">No sessions found</div>
-							</div>
+							<SessionNotFound />
 						</li>
 					{:else}
-					{#each sessions as session}
-						{@const isSelected = currentUrl.includes(`/projects/${data.project.id}/${session.id}`)}
-						<SidebarEntry
-							href={`/projects/${data.project.id}/${session.id}/commands`}
-							active={isSelected}
-						>
-							<div class="flex flex-col">
-								<span class="text-xl text-info">{session.name}</span>
-								<table class="text-left">
-									<tbody>
-										<tr>
-											<td>Created at:</td>
-											<td>{formatDateAndTime(session.createdAt)}</td>
-										</tr>
-										<tr>
-											<td>Last Heartbeat:</td>
-											<td
-												class={olderThanInSeconds(session.heartbeatAt, 3600)
-													? 'text-error'
-													: 'text-success'}>{formatDateAndTime(session.heartbeatAt)}</td
-											>
-										</tr>
-									</tbody>
-								</table>
-							</div>
-						</SidebarEntry>
-					{/each}
-				{/if}
-					{/await}
+						{#each sessions as session}
+							{@const isSelected = currentUrl.includes(`/projects/${project.id}/${session.id}`)}
+							<SidebarEntry
+								href={`/projects/${project.id}/${session.id}/commands`}
+								active={isSelected}
+							>
+								<Session
+									name={session.name}
+									createdAt={session.createdAt}
+									heartbeatAt={session.heartbeatAt}
+								/>
+							</SidebarEntry>
+						{/each}
+					{/if}
+				{/await}
 			</svelte:fragment>
 		</Sidebar>
 	</div>
