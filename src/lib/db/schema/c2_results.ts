d9294eccc5e7faebb2d299fd93180503bb9f380b
VipWW <55458813+VipWW@users.noreply.github.com>
2024-04-25 19:10:21 +0200

    feat: changed the primary key of the results table

--- src/lib/db/schema/c2_results.ts
+++ src/lib/db/schema/c2_results.ts
@@ -1,23 +1,29 @@
-import { pgTable, uuid, text, integer, timestamp } from 'drizzle-orm/pg-core';
+import { pgTable, uuid, text, integer, timestamp, primaryKey } from 'drizzle-orm/pg-core';
 import { C2Commands } from './c2_commands';
 import { relations, sql } from 'drizzle-orm';
 
-export const C2ResultChunks = pgTable('c2_result_chunks', {
-	command_id: uuid('command_id')
-		.notNull()
-		.references(() => C2Commands.id, { onDelete: 'cascade' }),
-	resultChunk: text('result_chunk').notNull().default(''),
-	chunkSize: integer('chunk_size')
-		.notNull()
-		.default(sql`length(result_chunk)`),
-	chunkOffset: integer('chunk_offset').notNull(),
-	createdAt: timestamp('created_at', {
-		withTimezone: true,
-		mode: 'date'
+export const C2ResultChunks = pgTable(
+	'c2_result_chunks',
+	{
+		command_id: uuid('command_id')
+			.notNull()
+			.references(() => C2Commands.id, { onDelete: 'cascade' }),
+		resultChunk: text('result_chunk').notNull().default(''),
+		chunkSize: integer('chunk_size')
+			.notNull()
+			.default(sql`length(result_chunk)`),
+		chunkOffset: integer('chunk_offset').notNull(),
+		createdAt: timestamp('created_at', {
+			withTimezone: true,
+			mode: 'date'
+		})
+			.notNull()
+			.default(sql`now()`)
+	},
+	(table) => ({
+		pk: primaryKey({ columns: [table.command_id, table.chunkOffset] })
 	})
-		.notNull()
-		.default(sql`now()`)
-});
+);
 
 export const c2ResultChunkRelations = relations(C2ResultChunks, ({ one }) => ({
 	Command: one(C2Commands, {
