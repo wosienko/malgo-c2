00a5ac38e07cf109278a2c1d11e534982bc17778
VipWW <55458813+VipWW@users.noreply.github.com>
2024-04-25 17:57:38 +0200

    feat: created new command component

---
+++ src/lib/components/custom/command/Command.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+	let isResultVisible = $state(true);
+	let isCommandVisible = $state(true);
+</script>
+
+<div class="mx-3 rounded-2xl border-2 border-neutral bg-base-100 p-4 shadow-xl">
+	<div>
+		<div class="mb-3 flex justify-between">
+			<h2 class="card-title">
+				<span class="badge badge-primary">COMMAND</span>
+				<span class="badge badge-secondary">CANCELED</span>
+			</h2>
+			<div class="flex flex-col justify-around space-y-1.5 md:flex-row md:space-x-1 md:space-y-0">
+				<button class="btn btn-sm" on:click={() => (isCommandVisible = !isCommandVisible)}
+					>Toggle command</button
+				>
+				<button class="btn btn-sm" on:click={() => (isResultVisible = !isResultVisible)}
+					>Toggle result</button
+				>
+				<button class="btn btn-sm">Cancel</button>
+			</div>
+		</div>
+		<p>Operator: Admin Admin</p>
+		<table class="w-full">
+			<tbody>
+				<tr>
+					<td class="text-left">Created:</td>
+					<td class="text-right">08.04.2024 12:15:36</td>
+				</tr>
+				<tr>
+					<td class="text-left">Last result update:</td>
+					<td class="text-right">08.04.2024 12:15:36</td>
+				</tr>
+			</tbody>
+		</table>
+		{#if isCommandVisible}
+			<div class="divider m-0"></div>
+			<p class="whitespace-pre-line">Command</p>
+		{/if}
+		{#if isResultVisible}
+			<div class="divider m-0"></div>
+			<p class="whitespace-pre-line">
+				result result result result result result result result result result result result result
+				result result result result result result result result result result result result result
+				result result result result result result result result result result result result result
+				result result result result result result result result result result result result result
+				result result result result result result result result result result result result result
+				result result result result
+			</p>
+		{/if}
+	</div>
+</div>
--- src/routes/(authorized)/projects/[id]/[sessionid]/commands/+page.svelte
+++ src/routes/(authorized)/projects/[id]/[sessionid]/commands/+page.svelte
@@ -1,10 +1,14 @@
-<div class="flex h-2/3 w-full justify-around">
-	<div class="m-3 flex-1">
-		<textarea class="textarea textarea-warning w-full resize-none" rows="9" placeholder="Command"
+<script>
+	import Command from '$lib/components/custom/command/Command.svelte';
+</script>
+
+<div class="flex h-1/2 w-full justify-around border-b-2 border-neutral pb-6">
+	<div class="m-3 flex h-full flex-1 flex-col">
+		<textarea class="textarea textarea-warning w-full grow resize-none" placeholder="Command"
 		></textarea>
 		<button class="btn btn-warning mt-3 w-full">Run</button>
 	</div>
-	<div class="m-3 hidden flex-1 overflow-auto md:block">
+	<div class="m-3 hidden h-full flex-1 overflow-auto md:block">
 		<table class="table table-fixed">
 			<thead>
 				<tr>
@@ -88,6 +92,6 @@
 		</table>
 	</div>
 </div>
-<div class="hidden h-1/3 w-full md:block">
-	<p>last command</p>
+<div class="h-1/2 w-full overflow-y-auto py-3">
+	<Command />
 </div>
