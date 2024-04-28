a24dbf423292fdf52ce404d8519327dc55d7e4f0
VipWW <55458813+VipWW@users.noreply.github.com>
2024-04-28 19:15:31 +0200

    fix: minor fixes

--- src/routes/(authorized)/projects/[id]/+page.svelte
+++ src/routes/(authorized)/projects/[id]/+page.svelte
@@ -1,1 +1,5 @@
-<p>this is the content</p>
+<div class="flex h-full flex-col items-center justify-center space-y-3">
+	<h1 class="text-4xl font-extrabold leading-none tracking-tight md:text-5xl lg:text-6xl">
+		Select a session
+	</h1>
+</div>
