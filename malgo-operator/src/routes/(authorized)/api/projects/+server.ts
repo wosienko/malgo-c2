5473600ea5d972cc47f6109488d9f3c7943539d1
VipWW <55458813+VipWW@users.noreply.github.com>
2024-05-05 16:12:08 +0200

    chore: API refactor, pt. 2

--- malgo-operator/src/routes/(authorized)/api/projects/+server.ts
+++ malgo-operator/src/routes/(authorized)/api/projects/+server.ts
@@ -1,20 +1,9 @@
 import type { RequestHandler } from './$types';
 import { json } from '@sveltejs/kit';
 import { getAllProjectsForOperator, getProjectsForOperator } from '$lib/services/project-service';
-import { isUserOperator } from '$lib/services/roles-service';
 
 export const GET: RequestHandler = async ({ url, locals }) => {
 	const userId = locals.user!.id;
-	if (!(await isUserOperator(userId))) {
-		return json(
-			{
-				message: 'You are not authorized to create a project'
-			},
-			{
-				status: 403
-			}
-		);
-	}
 
 	const page = Number(url.searchParams.get('page')) || 1;
 	const pageSize = Number(url.searchParams.get('pageSize')) || 10;
