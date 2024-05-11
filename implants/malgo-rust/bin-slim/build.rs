e62a4c4ca53134d2d5b7b91d5663ed3127feedeb
Piotr Wosiak <piotr.wosiak@gmail.com>
2024-05-11 21:50:20 +0200

    fix: cross-compilation fix

--- implants/malgo-rust/bin-slim/build.rs
+++ implants/malgo-rust/bin-slim/build.rs
@@ -1,6 +1,6 @@
 fn main() {
     // Linker arguments for building from Windows
-    if cfg!(target_os = "windows") {
+    if cfg!(target_env = "msvc") {
         println!("cargo:rustc-link-arg=/ENTRY:WinMain");
         println!("cargo:rustc-link-arg=/SUBSYSTEM:windows");
     }
