83f55ce57062d622280d17663612e59de0730c8d
Piotr Wosiak <piotr.wosiak@gmail.com>
2024-05-11 20:48:38 +0200

    fix: cross-compilation fix

--- implants/malgo-rust/bin-slim/build.rs
+++ implants/malgo-rust/bin-slim/build.rs
@@ -1,11 +1,11 @@
 fn main() {
     // Linker arguments for building from Windows
     if cfg!(target_os = "windows") {
-        println!("cargo:rustc-link-arg=/ENTRY:_start");
+        println!("cargo:rustc-link-arg=/ENTRY:WinMain");
         println!("cargo:rustc-link-arg=/SUBSYSTEM:windows");
     }
     // Linker arguments for GNU MinGW cross-compilation
     else {
-        println!("cargo:rustc-link-arg=-e _start");
+        println!("cargo:rustc-link-arg=-e WinMain");
     }
 }
--- implants/malgo-rust/bin-slim/src/main.rs
+++ implants/malgo-rust/bin-slim/src/main.rs
@@ -8,6 +8,11 @@ compile_error!("This program is only designed to run on Windows");
 use bin_slim::essentials::*;
 
 #[no_mangle]
-pub extern "C" fn _start() -> u32 {
+pub extern "C" fn WinMain(
+    _h_instance: u32,
+    _h_prev_instance: u32,
+    _lp_cmd_line: *const u8,
+    _n_cmd_show: i32,
+) -> u32 {
     panic!()
 }
