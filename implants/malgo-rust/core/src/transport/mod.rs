65e0b9dd96b46f1c62fefaecabda6fd71d9b9dd8
Piotr Wosiak <piotr.wosiak@gmail.com>
2024-05-12 16:14:45 +0200

    feat: decided to scrap no_std rust

--- implants/malgo-rust/core/src/transport/mod.rs
+++ implants/malgo-rust/core/src/transport/mod.rs
@@ -1,6 +1,6 @@
 use crate::session::Session;
 pub trait Transport {
-    fn register_session(&self, session: Session) -> Result<(), &str>;
+    fn register_session(&self, session: Session) -> Result<(), String>;
 }
 
 pub mod dns;
